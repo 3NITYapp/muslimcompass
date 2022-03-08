7935b87af80a5c160d43f975711fe4f9ded3ce94
ahsansuhail <ahsansuhail@evidentpoint.com>
2022-03-09 00:40:53 +0500

    - build 1.6

--- app/src/main/java/qiblacompass/qibladirection/finddirection/prayer/PrayerActivity.kt
+++ app/src/main/java/qiblacompass/qibladirection/finddirection/prayer/PrayerActivity.kt
@@ -1,24 +1,14 @@
 package qiblacompass.qibladirection.finddirection.prayer
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.ContentResolver
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
-import android.media.AudioAttributes
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import androidx.core.app.NotificationCompat
 import androidx.core.text.TextUtilsCompat
 import androidx.core.view.ViewCompat
 import androidx.lifecycle.ViewModelProvider
 import qiblacompass.qibladirection.finddirection.R
 import qiblacompass.qibladirection.finddirection.base.BaseActivity
-import qiblacompass.qibladirection.finddirection.compass.MainActivity
 import qiblacompass.qibladirection.finddirection.databinding.ActivityPrayerBinding
 import qiblacompass.qibladirection.finddirection.helper.Constants
 import qiblacompass.qibladirection.finddirection.helper.Constants.ALARM_FOR
@@ -27,7 +17,6 @@ import qiblacompass.qibladirection.finddirection.helper.DateUtils
 import qiblacompass.qibladirection.finddirection.helper.DialogUtils
 import qiblacompass.qibladirection.finddirection.helper.PrefsUtils
 import java.util.*
-
 
 class PrayerActivity : BaseActivity(), DialogUtils.OnSettingsSavedListener {
 
@@ -169,8 +158,6 @@ class PrayerActivity : BaseActivity(), DialogUtils.OnSettingsSavedListener {
             onBackPressed()
         }
 
-        sendNotification(this, "maghrib", "6:13 PM")
-
     }
 
     override fun onSettingsSaved(isSaved: Boolean) {
@@ -183,68 +170,6 @@ class PrayerActivity : BaseActivity(), DialogUtils.OnSettingsSavedListener {
             val intent = Intent(context, PrayerActivity::class.java)
             context.startActivity(intent)
         }
-    }
-
-    private fun sendNotification(applicationContext: Context, key: String, time: String) {
-        val utils = PrefsUtils(applicationContext)
-        var position = 0
-        for (i in Constants.KEYS.indices) {
-            if (Constants.KEYS[i] == key)
-                position = i
-        }
-        val title = Constants.NAME_ID[position]
-
-//        val soundUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + applicationContext.packageName + "/" + R.raw.azan)
-        val soundUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + applicationContext.packageName + "/" + R.raw.azan)
-
-        val id = generateRandom()
-
-        var intent = Intent()
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        intent = Intent(applicationContext, MainActivity::class.java)
-
-        val resultPendingIntent = PendingIntent.getActivity(
-            applicationContext, id , intent,
-            PendingIntent.FLAG_ONE_SHOT
-        )
-        val channelId = "Alarm-Pray"
-        val notificationBuilder =
-            NotificationCompat.Builder(applicationContext, channelId)
-                .setSmallIcon(R.drawable.app_icon)
-                .setColor(Color.TRANSPARENT)
-                .setAutoCancel(true)
-                .setContentTitle("Time $title")
-                .setContentText("Will start at $time")
-                .setSound(soundUri)
-                .setContentIntent(resultPendingIntent)
-        val mNotificationManager =
-            applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val importance = NotificationManager.IMPORTANCE_HIGH
-            val channel = NotificationChannel(
-                channelId,
-                applicationContext.getString(R.string.app_name), importance
-            )
-            val audioAttributes = AudioAttributes.Builder()
-                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                .setUsage(AudioAttributes.USAGE_NOTIFICATION)
-                .build()
-            channel.setSound(soundUri, audioAttributes)
-            channel.enableLights(true)
-            channel.lightColor = Color.RED
-            channel.enableVibration(true)
-            channel.vibrationPattern = longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 400)
-            notificationBuilder.setChannelId(channelId)
-            mNotificationManager.createNotificationChannel(channel)
-        }
-            mNotificationManager.notify(id, notificationBuilder.build())
-    }
-
-    private fun generateRandom(): Int {
-        val random = Random()
-        return random.nextInt(9999 - 1000) + 1000
     }
 
 }
